d4e57255d2c1d3577d1a53e0183f41942e1f62ad
Susmita277 <susmetaadangal@gmail.com>
2025-05-22 16:52:09 +0545

    contact page added

---
+++ resources/views/livewire/contact.blade.php
@@ -0,0 +1,82 @@
+
+  <div class="p-12 grid grid-cols-5">
+        <div class="col-span-4  bg-gray-100 rounded-xl p-8 pr-40 relative">
+            <h4>Send us a message</h4>
+            <p class="pr-40">Do you have a question ? A complaint ? Or need any help to choose the right product from
+                Zalomi.
+                Feeel free to contact us
+            </p>
+            <div class="grid grid-cols-2 gap-4 py-10 pr-20">
+                <div>
+                    <label>Full Name</label>
+                    <input type="name" placeholder="john doe"
+                        class="w-full p-2 my-2  border border-gray-200 dark:border-gray-700 rounded-md pr-10 outline-none"
+                        required />
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type="email" placeholder="john@gmail.com"
+                        class="w-full p-2 my-2  border border-gray-200 dark:border-gray-700 rounded-md pr-10 outline-none"
+                        required />
+                </div>
+                <div>
+                    <label>Mobile Number</label>
+                    <input type="text" placeholder="9864545008"
+                        class="w-full p-2 my-2  border border-gray-200 dark:border-gray-700 rounded-md pr-10 outline-none"
+                        required />
+                </div>
+                <div>
+                    <label>Address</label>
+                    <input type="text" placeholder="Birtamode-5,Jhapa"
+                        class="w-full p-2 my-2  border border-gray-200 dark:border-gray-700 rounded-md pr-10 outline-none"
+                        required />
+                </div>
+                <div class="col-span-2">
+                    <label>Message</label>
+                    <textarea name="description" id="" placeholder="Hey,iam john doe,i have a query ..."
+                        class="w-full p-2 my-2 max-h-20 min-h-20 border border-gray-200 dark:border-gray-700 rounded-md pr-10 outline-none">
+                    </textarea>
+                </div>
+
+
+            </div>
+            <div class="justify-end flex pr-20"><button class="mbtn">Send message</button></div>
+            <div class="bg-primary rounded-xl absolute right-0  bottom-0  w-[400px] h-fit translate-x-[62%] p-8 ">
+                <h5 class="text-white">Hi! We are always here to help you.</h5>
+                <div class="grid grid-cols-1 py-4 gap-4 border-b border-gray-100">
+                    <div class="bg-white  rounded-xl flex gap-5 p-5 items-center">
+                        <span x-hugeicon:headset class="w-8 h-8 text-black"></span>
+                        <div>
+                            <h5>Call Us/Whatsapp</h5>
+                            <p>9856778923</p>
+                        </div>
+                    </div>
+                    <div class="bg-white  rounded-xl flex gap-8 p-5 items-center">
+                        <span x-hugeicon:location class="w-8 h-8 text-black"></span>
+                        <div>
+                            <h5>Address:</h5>
+                            <p>Birtamode,5</p>
+                        </div>
+                    </div>
+                    <div class="bg-white  rounded-xl flex gap-8 p-5 items-center">
+                        <span x-hugeicon:mail class="w-8 h-8 text-black"></span>
+                        <div>
+                            <h5>Email:</h5>
+                            <p>johndoe@gmail.com</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="py-4 text-white">
+                    <h5>Connect with us</h5>
+                    <div class="flex gap-4 py-2">
+                        <span x-hugeicon:facebook class="w-6 h-6 "></span>
+                        <span x-hugeicon:snapchat class="w-6 h-6 "></span>
+                        <span x-hugeicon:tiktok class="w-6 h-6 "></span>
+                        <span x-hugeicon:instagram class="w-6 h-6 "></span>
+                         
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
