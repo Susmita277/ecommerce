c75bc8b880359b10b36c397e5ece0c8804509788
Susmita277 <susmetaadangal@gmail.com>
2025-05-25 17:15:07 +0545

    cart logic added in product

--- app/Livewire/Allproduct.php
+++ app/Livewire/Allproduct.php
@@ -2,37 +2,68 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
 use Livewire\Component;
 use App\Models\Product;
 use Livewire\Attributes\Url;
 use App\Models\Category;
 use Livewire\Withpagination;
+use Livewire\Navbar;
 
 class Allproduct extends Component
 {
     use WithPagination;
     #[Url]
-    public $selected_categories =[];
+    public $selected_categories = [];
+
 
     #[Url]
-      public $sortOrder = 'low'; // default
+    public $sortOrder = 'low'; // default
 
-    public function setSortOrder($order)
+    public $cart = [];
+
+    public function mount()
     {
-        $this->sortOrder = $order;
+        $this->cart = CartManagement::get();
     }
+
+
+public function addToCart($productId)
+{
+    $alreadyInCart = collect($this->cart)->pluck('product_id')->contains($productId);
+
+    if ($alreadyInCart) {
+        $this->dispatch('cart-toast', [
+            'message' => 'Already in cart',
+            'type' => 'warning',
+        ]);
+        return;
+    }
+
+    \App\Helpers\CartManagement::addToCart($productId);
+    $this->cart = \App\Helpers\CartManagement::get();
+
+    $this->dispatch('cart-toast', [
+        'message' => 'Added to cart',
+        'type' => 'success',
+    ]);
+}
+
+
+
+
     public function render()
     {
-        $productQuery= Product::query()->where('is_active',1);
+        $productQuery = Product::query()->where('is_active', 1);
 
-        if(!empty($this->selected_categories)){
-            $productQuery->whereIn('category_id',$this->selected_categories);
+        if (!empty($this->selected_categories)) {
+            $productQuery->whereIn('category_id', $this->selected_categories);
         }
 
         $categories = Category::all();
-        return view ('livewire.allproduct',[
-            'products' =>$productQuery->paginate(4),
-            'categories'=>$categories,
+        return view('livewire.allproduct', [
+            'products' => $productQuery->paginate(4),
+            'categories' => $categories,
         ]);
     }
 }
