53179e1587f18af4b62d5b3c09ecc528bb65ef99
Susmita277 <susmetaadangal@gmail.com>
2025-05-26 23:12:05 +0545

    register page added

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -3,42 +3,34 @@
 namespace App\Livewire\Auth;
 
 use App\Models\User;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
-use Livewire\Attributes\Layout;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
-#[Layout('components.layouts.auth')]
 class Register extends Component
 {
-    public string $name = '';
+    public $name, $email, $password, $password_confirmation;
 
-    public string $email = '';
-
-    public string $password = '';
-
-    public string $password_confirmation = '';
-
-    /**
-     * Handle an incoming registration request.
-     * p
-     */
-    public function register(): void
+    public function register()
     {
-        $validated = $this->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
+        $this->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8',
         ]);
 
-        $validated['password'] = Hash::make($validated['password']);
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
 
-        event(new Registered(($user = User::create($validated))));
-
-        Auth::login($user);
-
-        $this->redirect(route('dashboard', absolute: false), navigate: true);
+        $this->dispatch('close-register');
+        $this->dispatch('open-login');
+    
+    }
+    public function render()
+    {
+        return view('livewire.auth.register');
     }
 }
