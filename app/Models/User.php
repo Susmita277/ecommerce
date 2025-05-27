cb3fc033c04b5329a1e25148cf9d321cd144a13d
Susmita277 <susmetaadangal@gmail.com>
2025-05-27 14:32:58 +0545

    implement filament users and provide access to admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -47,20 +49,12 @@ class User extends Authenticatable
         ];
     }
 
-    /**
-     * Get the user's initials
-     */
-    public function initials(): string
-    {
-        return Str::of($this->name)
-            ->explode(' ')
-            ->map(fn (string $name) => Str::of($name)->substr(0, 1))
-            ->implode('');
-    }
 
-     public function canAccessFilament(): bool
-{
-    return true;
-}
+     public function canAccessPanel(Panel $panel): bool
+    {
+      
+        return $this->email == 'admin@gmail.com';
+    }
+  
 
 }
