e2f03fa431ea64603e4a596bb829457c3896a03c
Susmita277 <susmetaadangal@gmail.com>
2025-05-27 23:36:26 +0545

    enable add to cart method from homepage

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -4,19 +4,51 @@ namespace App\Livewire;
 
 use App\Models\Category;
 use Livewire\Component;
+use App\Helpers\CartManagement;
 use App\Models\Product;
 
 class Home extends Component
-{  
-   
+{
+    public $cart = [];
+
+    public function mount()
+    {
+        $this->cart = CartManagement::get();
+    }
+
+    public function addToCart($productId)
+    {
+        $alreadyInCart = collect($this->cart)->pluck('product_id')->contains($productId);
+
+        if ($alreadyInCart) {
+            $this->dispatch('cart-toast', [
+                'message' => 'Already in cart',
+                'type' => 'warning',
+            ]);
+            return;
+        }
+
+        \App\Helpers\CartManagement::addToCart($productId);
+        $this->cart = \App\Helpers\CartManagement::get();
+
+        $this->dispatch('cart-toast', [
+            'message' => 'Added to cart',
+            'type' => 'success',
+        ]);
+    }
+
 
     public function render()
     {
         $newArrivals = Product::latest()->take(4)->get();
-        $categories = Category::where('is_active',1)->get();
+        $categories = Category::where('is_active', 1)->get();
         $topProducts = Product::oldest()->take(4)->get();
 
-
-        return view('livewire.home', compact('newArrivals', 'categories', 'topProducts'));
+        return view('livewire.home', [
+            'newArrivals' => $newArrivals,
+            'categories' => $categories,
+            'topProducts' => $topProducts,
+            'cart' => $this->cart,
+        ]);
     }
 }
