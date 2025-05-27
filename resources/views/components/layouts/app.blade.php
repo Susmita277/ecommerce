14501bcbeaf489251867d028b2dd2fe6f01d3fb3
Susmita277 <susmetaadangal@gmail.com>
2025-05-27 23:36:49 +0545

    add loader animation

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -11,40 +11,54 @@
 </head>
 
 <body class="text-black">
+    <div id="preloader" class="fixed inset-0 bg-white z-[9999] flex items-center justify-center">
+        <div class="animate-spin h-12 w-12 border-4 border-[var(--color-primary)] border-t-transparent rounded-full">
+        </div>
+    </div>
     <x-layouts.partials.header />
     {{ $slot }}
     <x-layouts.partials.footer />
 
- <div class="toast absolute top-40 left-1/2 z-100">
-    <div id="cart-toast" class="alert hidden bg-green-500 text-white">
-        <span id="cart-toast-message"></span>
+    <div class="toast absolute top-40 left-1/2 z-100">
+        <div id="cart-toast" class="alert hidden bg-green-500 text-white">
+            <span id="cart-toast-message"></span>
+        </div>
     </div>
-</div>
-<div class="toast toast-top toast-center z-[9999] fixed" id="toast-container">
-    <div id="cart-toast" class="alert hidden">
-        <span id="cart-toast-message"></span>
+    <div class="toast toast-top toast-center z-[9999] fixed" id="toast-container">
+        <div id="cart-toast" class="alert hidden">
+            <span id="cart-toast-message"></span>
+        </div>
     </div>
-</div>
 
-<script>
-    Livewire.on('cart-toast', ({ message, type = 'success' }) => {
-        const toast = document.getElementById('cart-toast');
-        const toastMessage = document.getElementById('cart-toast-message');
-        const toastContainer = document.getElementById('toast-container');
-
-        toastMessage.textContent = message;
-
-        toast.className = 'alert alert-' + type;
-        toastContainer.style.display = 'block';
-
+    <script>
+            window.addEventListener('load', () => {
+        const preloader = document.getElementById('preloader');
+        preloader.classList.add('opacity-0');
         setTimeout(() => {
-            toastContainer.style.display = 'none';
-        }, 2000);
+            preloader.style.display = 'none';
+        }, 500); // matches transition duration if you add one
     });
-</script>
+    
+        Livewire.on('cart-toast', ({
+            message,
+            type = 'success'
+        }) => {
+            const toast = document.getElementById('cart-toast');
+            const toastMessage = document.getElementById('cart-toast-message');
+            const toastContainer = document.getElementById('toast-container');
+
+            toastMessage.textContent = message;
+
+            toast.className = 'alert alert-' + type;
+            toastContainer.style.display = 'block';
+
+            setTimeout(() => {
+                toastContainer.style.display = 'none';
+            }, 2000);
+        });
+    </script>
 
 
 </body>
 
 </html>
-
