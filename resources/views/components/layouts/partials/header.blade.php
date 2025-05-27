46c6b0da35152cda8010495f2ce0d8aaf1dcb46d
Susmita277 <susmetaadangal@gmail.com>
2025-05-27 14:37:31 +0545

    add login/loggout functionality and create accessbility for guest and authentic users

--- resources/views/components/layouts/partials/header.blade.php
+++ resources/views/components/layouts/partials/header.blade.php
@@ -40,30 +40,52 @@
         </li>
 
 
-        <li class="flex gap-3 items-center ">
-            <label for="my-drawer-4"
-                class="rounded-full p-2 flex items-center justify-center bg-[var(--color-accent)] relative cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="#ffffff" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                </svg>
-
-                @if (count($cart ?? []) > 0)
-                    <span
-                        class="absolute -top-3 p-1 -right-1 w-2 h-2 bg-red-500 rounded-full flex text-white items-center justify-center">{{ count($cart ?? []) }}</span>
-                @endif
+        <li class="flex gap-5 items-center ">
+            <label for="my-drawer-4" class="cursor-pointer flex gap-1 items-center">
+                <h5>Cart</h5>
+                <h5 class="text-blue-900">(0)</h5>
             </label>
+            @guest
+                <!-- Trigger Button -->
+                <div class="flex gap-2 items-center cursor-pointer">
+                    <div class="rounded-full p-2 flex items-center justify-center bg-gray-200 "
+                        onclick="login_modal.showModal()">
+                        <button onclick="window.dispatchEvent(new CustomEvent('open-login'))" class="outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="#000000" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                            </svg></button>
+                    </div>
+                    <div>
+                        <h5>Account</h5>
+                        <div>
+                            <span class="text-primary cursor-pointer" onclick="window.dispatchEvent(new CustomEvent('open-login'))">sign in/</span>
+                            <span class="text-primary" onclick="window.dispatchEvent(new CustomEvent('open-register'))">create</span>
+                        </div>
+                    </div>
+                </div>
 
-            <!-- Trigger Button -->
-            <div class="rounded-full p-2 flex items-center justify-center bg-accent " onclick="login_modal.showModal()">
-                <button onclick="window.dispatchEvent(new CustomEvent('open-login'))" class="outline-none">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="#ffffff" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-                </svg></button>
-            </div>
+            @endguest
+
+            @auth
+                <div class="dropdown">
+                    <div class="gap-2 flex items-center">
+                        <div tabindex="0" class="userbtn m-1">
+                            {{ substr(auth()->user()->name, 0, 1) }}
+                        </div>
+                        <div>
+                            <h5>Account</h5>
+                            <span class="text-primary">{{ auth()->user()->name }}</span>
+                        </div>
+                    </div>
+                    <ul tabindex="0" class="dropdown-content menu bg-white  rounded-box z-10 w-56 p-2 shadow-sm">
+                        <li><a>My Accounts</a></li>
+                        <li><a>My Orders</a></li>
+                        <li><a href="/logout">Logout</a></li>
+                    </ul>
+                </div>
+            @endauth
 
 
         </li>
@@ -121,5 +143,3 @@
 @livewire('cart-manager')
 @livewire('auth.login')
 @livewire('auth.register')
-
-
