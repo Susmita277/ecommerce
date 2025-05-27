6d42d442904d6b7990a442c52e4da2a24bd3da07
Susmita277 <susmetaadangal@gmail.com>
2025-05-27 23:35:44 +0545

    validate checkout method

--- app/Livewire/Checkoutpage.php
+++ app/Livewire/Checkoutpage.php
@@ -7,9 +7,22 @@ use App\Helpers\CartManagement;
 
 class Checkoutpage extends Component
 {
+    public $city;
+    public $address;
+    public $payment_method = 'cash';
+    public function placeorder(){
+      $this->validate([
+        'city'=> 'required',
+        'address'=> 'required',
+        'payment_method'=> 'required',
+      ]);
+    }
+
     public $cart_items = [];
     public $grand_total;
+    
     public function mount(){
+        $this->payment_method = 'cash';
         $this->cart_items = CartManagement::getCartItemsFromCookie();
         $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
 
