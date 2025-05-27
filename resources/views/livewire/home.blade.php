70bd537f3fd38f7e7491fe5c3f38c69d2a0ce929
Susmita277 <susmetaadangal@gmail.com>
2025-05-27 23:39:01 +0545

    enable cart

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,8 +1,8 @@
 <div>
     <x-partials.herosection />
     <x-partials.overview />
-    <x-partials.new-arrivals :new-arrivals="$newArrivals" />
+    <x-partials.new-arrivals :new-arrivals="$newArrivals" :cart="$cart"/>
     <x-partials.category :categories="$categories" />
-    <x-partials.topproducts :top-products="$topProducts" />
+    <x-partials.topproducts :top-products="$topProducts" :cart="$cart" />
 </div>
 
